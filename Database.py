2dffd3a19d1ad5fba935673c0e3e0b144f5df2f8
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2024-04-09 00:25:18 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://PAGAL:NallaPassword%40123@localhost:3306/sqlalchemy"
+URL_DATABASE = ""
 
 engine = create_engine(URL_DATABASE)
 
